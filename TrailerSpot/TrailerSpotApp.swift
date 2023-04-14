da154a35e634999f96ce553717f42eafca0b64e7
Robert <90681378+robiv2007@users.noreply.github.com>
2023-04-14 11:01:50 +0200

    Initial Commit

---
+++ TrailerSpot/TrailerSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailerSpotApp.swift
+//  TrailerSpot
+//
+//  Created by vatran robert on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct TrailerSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
