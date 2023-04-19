45445119015907a4407e8ef52aed7ef60e02a421
Robert <90681378+robiv2007@users.noreply.github.com>
2023-04-19 15:18:25 +0200

    Added viewBuilder for the grids.

--- TrailerSpot/MovieList/MovieListView.swift
+++ TrailerSpot/MovieList/MovieListView.swift
@@ -11,70 +11,76 @@ import Domain
 
 struct MovieListView: View {
     @StateObject var vm = MovieListViewModel()
-
-
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
             ScrollView {
                 VStack(alignment: .leading, spacing: 8) {
-                Text("Upcoming Movies")
+                    Text("Upcoming Movies")
                         .modifier(TextCategories())
                         .font(.title)
-                    ScrollView(.horizontal){
-                        LazyHGrid(rows: vm.rows, spacing: 20){
-                            ForEach(vm.upcomingMovies, id: \.id){ item in
-                                NavigationLink(destination: MovieDetailView(movie: item)){
-                                    image(path: item.posterPath)
-                                        .frame(width: 200, height: 300)
-                                }
-                            }
-                        }
-                    }
+                    horizontalGrid()
                 }
                 Text("Popular")
                     .font(.title)
                     .modifier(TextCategories())
-                LazyVGrid(columns: vm.columns, spacing: 20) {
-                    ForEach(vm.movie, id: \.id) { movie in
-                        NavigationLink(destination: MovieDetailView(movie: movie)) {
-                            image(path: movie.posterPath)
-                                .frame(width: 120, height: 180)
-                        }
+                verticalGrid()
+                    .onAppear {
+                        vm.fetchMovies()
+                        vm.fetchUpcomingMovies()
                     }
-                }
-                .onAppear {
-                    vm.fetchMovies()
-                    vm.fetchUpcomingMovies()
-                }
             }
-
             .scrollIndicators(.hidden)
-
         }
         .preferredColorScheme(.dark)
         .padding(16)
     }
-
+    
     @ViewBuilder
-    private func image(path: String) -> some View {
-            AsyncImage(url: URL(string: "\(vm.imageUrl)\(path )")) {
-                $0.resizable()
-                    .cornerRadius(10)
-            }placeholder: {
-                ProgressView()
+    private func horizontalGrid() -> some View {
+        ScrollView(.horizontal){
+            LazyHGrid(rows: vm.rows, spacing: 20){
+                ForEach(vm.upcomingMovies, id: \.id){ item in
+                    NavigationLink(destination: MovieDetailView(movie: item)){
+                        image(path: item.posterPath)
+                            .frame(width: 200, height: 300)
+                    }
+                }
+            }
         }
     }
-
+    
+    @ViewBuilder
+    private func verticalGrid() -> some View {
+        LazyVGrid(columns: vm.columns, spacing: 20) {
+            ForEach(vm.movie, id: \.id) { movie in
+                NavigationLink(destination: MovieDetailView(movie: movie)) {
+                    image(path: movie.posterPath)
+                        .frame(width: 120, height: 180)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func image(path: String) -> some View {
+        AsyncImage(url: URL(string: "\(vm.imageUrl)\(path )")) {
+            $0.resizable()
+                .cornerRadius(10)
+        }placeholder: {
+            ProgressView()
+        }
+    }
+    
     struct TextCategories: ViewModifier {
         let font = Font.system(size: 24).weight(.semibold)
         func body(content: Content) -> some View {
             content
-                .font(.system(size: 20))
                 .font(font)
                 .padding()
                 .frame(maxWidth: .infinity, maxHeight: 40, alignment: .leading)
                 .background(.thinMaterial)
-                .cornerRadius(5)
+                .cornerRadius(50)
         }
     }
 }
